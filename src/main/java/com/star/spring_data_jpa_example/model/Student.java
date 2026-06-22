9ae60c0107d4a357a175339a66b86ec957d83f56
Hardi <hardivirani2003@gmail.com>
2026-06-21 21:19:45 -0400

    first commit

---
+++ src/main/java/com/star/spring_data_jpa_example/model/Student.java
@@ -0,0 +1,56 @@
+package com.star.spring_data_jpa_example.model;
+
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.context.annotation.Scope;
+import org.springframework.stereotype.Component;
+
+
+@Component
+@Scope("prototype")
+public class Student {
+
+    //    @Value("25")
+    private int rollNo;
+
+    //    @Value("Hardi")
+    private String name;
+    private int marks;
+
+    public int getRollNo() {
+        return rollNo;
+    }
+
+    public void setRollNo(int rollNo) {
+        this.rollNo = rollNo;
+    }
+
+
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+
+
+    public int getMarks() {
+        return marks;
+    }
+
+    public void setMarks(int marks) {
+        this.marks = marks;
+    }
+
+
+    @Override
+    public String toString() {
+        return "Student{" +
+                "rollNo=" + rollNo +
+                ", name='" + name + '\'' +
+                ", marks=" + marks +
+                '}';
+    }
+}
